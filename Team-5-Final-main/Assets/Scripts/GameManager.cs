455a841f2d74639f1b2cbd4e355d0fd36d828190
triggerbruh <alessanhernandez@my.lonestar.edu>
2025-05-01 23:28:28 -0500

    Add files via upload
    Added some sound effects for shooting, taking damage, power ups, and dying.

---
+++ Team-5-Final-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Using for Scene Changes, UI, and game pausing
+    //Empty for now, I, Sora Vasquez, am working on this slowly but surely n.n (this will be deleted later) 
+    
+}
